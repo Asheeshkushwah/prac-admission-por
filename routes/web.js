bfa67316dcd9b8b048b19c70323fc31133a658c4
asheesh kushwah <asheesh94065@gmail.com>
2024-09-05 21:42:43 +0530

    yt

--- routes/web.js
+++ routes/web.js
@@ -8,7 +8,7 @@ const route = express.Router()
 
 route.get('/home', FrontController.home)
 route.get('/about', FrontController.about)
-route.get('/', FrontController.login)
+route.get('/login', FrontController.login)
 route.get('/register', FrontController.register)
 route.get('/contact', FrontController.contact)
 
